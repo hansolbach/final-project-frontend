d3a18c73178f90ec11d8de950a93296e33bd15cf
hansolbach <hansolbach@gmail.com>
2017-01-15 17:19:52 -0500

    make changes for INDEX.html

--- README.txt
+++ README.txt
@@ -1,2 +1,4 @@
+Link to the project https://hansolbach.github.io/frontend/INDEX.html
+
 Back End https://github.com/hansolbach/backend
 Front End https://github.com/hansolbach/frontend
---
+++ main.js
@@ -0,0 +1,21 @@
+document.querySelector('img').onclick = function() {
+    alert('Stop poking Ben!');
+}
+
+alert('Do you love Ben like we do?');
+
+/* 
+I look forward to learning from the final project
+*/
+
+/*
+
+function sayDontpoke() {
+	alert('dont poke the image');
+}
+
+document.querySelector('img).onclock = sayDontpoke
+
+call a function
+sayDontPoke()
+*/
