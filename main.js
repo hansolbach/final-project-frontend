2560d959802276264c1fb6a2171a552d69c5993d
Brendan Roach <brendan.roach.6@gmail.com>
2017-01-20 14:05:25 -0500

    changes to clean up CSS

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-document.querySelector('img').onclick = function() {
-    alert('This is a real picture!');
-}
-
-alert('Do you think Climate Change is a lie? Think Again, and here are our evidences!');
-
+var navBar = document.querySelector(".nav")
 /* 
 I look forward to learning from the final project
-*/
-
-/*
-
-function sayDontpoke() {
-	alert('dont poke the image');
-}
-
-document.querySelector('img).onclock = sayDontpoke
-
-call a function
-sayDontPoke()
 */
